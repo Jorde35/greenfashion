2f2c4530565d9938e4128c96e4b3a2ae4b1a4e53
Jorde35 <jorgecpa4675@gmail.com>
2023-11-30 23:06:08 -0300

    summary

--- MGFFrontend-main/src/app/global.ts
+++
@@ -1,3 +0,0 @@
-// globals.ts
-export const BackUrl: string = "http://localhost/MyGreenFashion/Backend/public/";
-export const ImgUrl: string = "http://localhost/MyGreenFashion/Backend/storage/app/public/";
